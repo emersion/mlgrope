cff11e8766bad72f23fdd8cf41c8eb44bb9e494e
emersion <contact@emersion.fr>
2017-05-02 16:48:28 +0200

    frontend: draw goal, try to draw rope

--- frontend.ml
+++ frontend.ml
@@ -7,31 +7,60 @@ open Backend
 
 let tick_rate = 1. /. 60.
 
+let ball_color = Graphics.black
+let bubble_color = Graphics.red
+let rope_color = Graphics.green
+let goal_color = Graphics.blue
+
+let mix v1 v2 t =
+	{ x = t *. v1.x +. (1. -. t) *. v2.x; y = t *. v1.y +. (1. -. t) *. v2.y }
+
+let int_of_position p =
+	(int_of_float p.x, int_of_float p.y)
+
 let draw_ball (b : ball) =
-	Graphics.set_color Graphics.black;
-	Graphics.fill_circle (int_of_float b.position.x) (int_of_float b.position.y) (int_of_float ball_radius)
+	let (x, y) = int_of_position b.position in
+	Graphics.set_color ball_color;
+	Graphics.fill_circle x y (int_of_float Mlgrope.ball_radius)
 
 let draw_bubble (b : bubble) =
-	Graphics.set_color Graphics.red;
-	Graphics.draw_circle (int_of_float b.position.x) (int_of_float b.position.y) (int_of_float b.radius)
+	let (x, y) = int_of_position b.position in
+	Graphics.set_color bubble_color;
+	Graphics.draw_circle x y (int_of_float b.radius)
 
 let draw_rope (r : rope) =
-	Graphics.set_color Graphics.green;
-	Graphics.draw_circle (int_of_float r.position.x) (int_of_float r.position.y) (int_of_float r.radius)
+	let (x, y) = int_of_position r.position in
+	Graphics.set_color rope_color;
+	Graphics.draw_circle x y (int_of_float r.radius)
+
+let draw_goal (g : goal) =
+	let (x, y) = int_of_position g.position in
+	Graphics.set_color goal_color;
+	Graphics.fill_rect (x - 1) (y - 1) 2 2
 
 let draw_entity e =
 	match e with
 	| Bubble(b) -> draw_bubble b
 	| Rope(r) -> draw_rope r
+	| Goal(g) -> draw_goal g
 
-let rec draw_entities l =
+let rec draw_link b l =
 	match l with
-	| e::l -> draw_entity e; draw_entities l
-	| [] -> ()
+	| Rope({position}) -> let a = 0.1 in
+		let n = 10 in
+		let line = Array.init n (fun i ->
+			let t = (float_of_int i) /. (float_of_int n) in
+			int_of_position (mix position b.position t)
+			(* TODO *)
+		) in
+		Graphics.set_color Graphics.black;
+		Graphics.draw_poly_line line
+	| _ -> ()
 
 let draw s =
 	draw_ball s.ball;
-	draw_entities s.entities
+	List.iter draw_entity s.entities;
+	List.iter (draw_link s.ball) s.ball.links
 
 let step g =
 	let t = Unix.gettimeofday () in
@@ -44,6 +73,7 @@ let step g =
 	g
 
 let () =
+	let rope = {position = {x = 200.; y = 100.}; radius = 40.} in
 	let g = {
 		size = {x = 400.; y = 400.};
 		time = Unix.gettimeofday ();
@@ -52,11 +82,12 @@ let () =
 				position = {x = 200.; y = 300.};
 				speed = {x = 0.; y = 0.};
 				accel = {x = 0.; y = -20.};
-				links = [];
+				links = [Rope(rope)];
 			};
 			entities = [
 				Bubble{position = {x = 100.; y = 200.}; radius = 50.};
-				Rope{position = {x = 200.; y = 100.}; radius = 40.};
+				Rope(rope);
+				Goal{position = {x = 200.; y = 20.}};
 			];
 		};
 	} in
