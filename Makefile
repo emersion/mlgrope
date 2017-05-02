1920f86c880394989bbac7faeac62d768909cfd9
Raphael Cornet <rcorne1@a-018739.ups.u-psud.fr>
2017-05-02 14:34:39 +0200

    fusion

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	ocamlc -thread -o mlgrope graphics.cma unix.cma threads.cma mlgrope.mli backend.ml frontend.ml mlgrope.ml
+	ocamlc -thread -o mlgrope graphics.cma unix.cma threads.cma mlgrope.mli backend.mli backend.ml frontend.ml mlgrope.ml
--- backend.ml
+++ backend.ml
@@ -6,18 +6,49 @@ let b  = {x = 4.2; y = 0.3}
 let (+) v1 v2 =
 	{ x = v1.x +. v2.x; y = v1.y +. v2.y }
 
-let print_vector v =
-	print_string "x = "; print_float v.x;
-	print_string " y = "; print_float v.y
-;;
+let (=) v1 v2 =
+	v1.x = v2.x && v1.y = v2.y 
 
-let ball_move b =
+let (-) v1 v2 =
+	{ x = v1.x -. v2.x; y = v1.y -. v2.y }
+
+
+let dot v1 v2  =  
+	{ x = v1.x *. v2.x; y = v1.y *. v2.y }
+
+let ( * ) s v  = 
+	{ x = s *. v.x; y = s *. v.y }
+
+let print_vector v =
+	print_string ("x = "); print_float v.x;
+	print_string " y = "; print_float v.y
+
+
+let ball_move b dt =
 	(* Compute new pos *)
-	let nspeed = b.speed + b.accel in
-	{ b with speed  = nspeed; position = b.position + nspeed}
+	let nspeed = b.speed + dt * b.accel in
+	let newB = { b with speed  = nspeed; position = b.position + nspeed } in
 
 	(* Check for collision*)
 
 	(* Respond to collision *)
 
 	(* Update position & speed *)
+	newB
+
+let check_collision b ent =
+	let ball_radius = 10.0 in 
+		match ent with
+		| Bubble(bu) ->
+			let dist = (bu.position.x -. b.position.x)**2.0  +. (bu.position.y -. b.position.y)**2.0 in
+			(ball_radius +. bu.radius)**2.0 >= dist
+		| _ -> false
+
+
+let rec check_collisions b entl =
+	(* let ball_radius = 10.0 in *)
+		match entl with
+		| [] -> false
+		| e::s -> (check_collision b e) || (check_collisions b s) 
+
+let move g dt = g
