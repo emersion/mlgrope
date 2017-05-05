fcd40e7479515d50eadb4e3ba85fe17fce2cf30c
emersion <contact@emersion.fr>
2017-05-05 15:27:53 +0200

    main: editor bootstrap

--- main.ml
+++ main.ml
@@ -1,11 +1,12 @@
+open Arg
 open Unix
 
 open Mlgrope
 open Frontend
 open Level
 
-let () =
-	let ch = open_in "levels/0.csv" in
+let play path =
+	let ch = open_in path in
 	let state = Level.input ch in
 	let g = {
 		size = {x = 500.; y = 500.};
@@ -14,3 +15,17 @@ let () =
 		state;
 	} in
 	Frontend.run g
+
+let edit path =
+	let ch = open_in path in
+	let state = Level.input ch in
+	(* TODO *)
+	raise Exit
+
+let () =
+	Arg.parse [
+		("-edit", Arg.String(edit), "Edit a level")
+	] play "Usage: mlgrope [-edit] level.csv";
+
+	(* No level specified *)
+	play "levels/0.csv"
